75c1155444d6f345c4edf84481ddf3d6410131a1
Inparsian <im@inpr.sn>
2025-06-27 10:26:12 -0400

    feat: Add workspaces to Hyprland singleton

--- src/singletons/hyprland/mod.rs
+++ src/singletons/hyprland/mod.rs
@@ -2,9 +2,9 @@ use futures_signals::signal::Mutable;
 use once_cell::sync::Lazy;
 use hyprland::{
     async_closure,
+    data::{Client, Workspace, Workspaces},
     event_listener::AsyncEventListener,
-    data::{Client, Workspace},
-    shared::{HyprDataActive, HyprDataActiveOptional}
+    shared::{HyprData, HyprDataActive, HyprDataActiveOptional}
 };
 
 // Wrapper structs to work with Hyprland data reactively
@@ -12,6 +12,7 @@ use hyprland::{
 pub struct Hyprland {
     pub active_client: Mutable<Option<Client>>,
     pub active_workspace: Mutable<Option<Workspace>>,
+    pub workspaces: Mutable<Option<Workspaces>>,
 }
 
 pub static HYPRLAND: Lazy<Hyprland> = Lazy::new(Hyprland::default);
@@ -34,19 +35,32 @@ pub fn refresh_active_workspace() {
     }
 }
 
+pub fn refresh_workspaces() {
+    let workspaces = Workspaces::get();
+    if let Ok(workspaces) = workspaces {
+        HYPRLAND.workspaces.set(Some(workspaces));
+    } else {
+        HYPRLAND.workspaces.set(None);
+    }
+}
+
 pub fn activate() {
     refresh_active_client();
     refresh_active_workspace();
+    refresh_workspaces();
 
     tokio::spawn(async move {
         let mut event_listener = AsyncEventListener::new();
 
-        event_listener.add_workspace_changed_handler(async_closure! { |_| refresh_active_workspace() });
         event_listener.add_window_closed_handler(async_closure! { |_| refresh_active_client() });
         event_listener.add_active_window_changed_handler(async_closure! { |_| refresh_active_client() });
         event_listener.add_float_state_changed_handler(async_closure! { |_| refresh_active_client() });
         event_listener.add_window_title_changed_handler(async_closure! { |_| refresh_active_client() });
         event_listener.add_fullscreen_state_changed_handler(async_closure! { |_| refresh_active_client() });
+        event_listener.add_workspace_added_handler(async_closure! { |_| refresh_workspaces() });
+        event_listener.add_workspace_deleted_handler(async_closure! { |_| refresh_workspaces() });
+        event_listener.add_workspace_moved_handler(async_closure! { |_| refresh_workspaces() });
+        event_listener.add_workspace_changed_handler(async_closure! { |_| refresh_active_workspace() });
 
         let _ = event_listener.start_listener_async().await;
     });
