b5fd65c0e3485118dab87d1e9d781bcb55e8c694
Inparsian <im@inpr.sn>
2025-07-15 19:48:37 -0400

    feat(translate): Add language select buttons & swap button

--- src/widgets/sidebar_left/modules/translate/mod.rs
+++ src/widgets/sidebar_left/modules/translate/mod.rs
@@ -7,13 +7,47 @@ use crate::singletons::g_translate::{language::{self, Language}, result::GoogleT
 static WORKER_TIMEOUT: Lazy<Mutex<Option<gtk4::glib::SourceId>>> = Lazy::new(|| Mutex::new(None));
 static WORKING: Lazy<Mutex<bool>> = Lazy::new(|| Mutex::new(false));
 
+static SOURCE_LANG: Lazy<Mutex<Option<Language>>> = Lazy::new(|| Mutex::new(language::get_by_name("English")));
+static TARGET_LANG: Lazy<Mutex<Option<Language>>> = Lazy::new(|| Mutex::new(language::get_by_name("Spanish")));
+static AUTO_DETECTED_LANG: Lazy<Mutex<Option<Language>>> = Lazy::new(|| Mutex::new(None));
+
 enum TranslationEvent {
     TranslationStarted,
     TranslationFinished(Result<GoogleTranslateResult, String>),
+    SourceLanguageChanged(Option<Language>),
+    TargetLanguageChanged(Option<Language>),
 }
 
 fn is_working() -> bool {
     WORKING.try_lock().map(|w| *w).unwrap_or(true)
+}
+
+fn change_source_language(lang: Option<Language>, tx: &async_channel::Sender<TranslationEvent>) {
+    let mut source_lang = SOURCE_LANG.lock().unwrap();
+    *source_lang = lang.clone();
+
+    tokio::spawn({
+        let lang = lang.clone();
+        let tx = tx.clone();
+
+        async move {
+            tx.send(TranslationEvent::SourceLanguageChanged(lang)).await.ok();
+        }
+    });
+}
+
+fn change_target_language(lang: Option<Language>, tx: &async_channel::Sender<TranslationEvent>) {
+    let mut target_lang = TARGET_LANG.lock().unwrap();
+    *target_lang = lang.clone();
+    
+    tokio::spawn({
+        let lang = lang.clone();
+        let tx = tx.clone();
+
+        async move {
+            tx.send(TranslationEvent::TargetLanguageChanged(lang)).await.ok();
+        }
+    });
 }
 
 async fn translate_future(
@@ -46,16 +80,39 @@ async fn translate_future(
 }
 
 pub fn new() -> gtk4::Box {
-    // TODO: Make these mutable by the user
-    let source_lang = language::get_by_name("English");
-    let target_lang = language::get_by_name("Spanish");
-
     let input_buffer = gtk4::TextBuffer::new(None);
     let output_buffer = gtk4::TextBuffer::new(None);
+    let from_to_button_transition_provider = gtk4::CssProvider::new();
 
     let (tx, rx) = async_channel::bounded::<TranslationEvent>(1);
 
     relm4_macros::view! {
+        source_lang_button = gtk4::Button {
+            set_label: SOURCE_LANG.lock().unwrap().as_ref().map_or("Source...", |l| &l.name),
+            set_css_classes: &["google-translate-language-select-button", "source-lang"],
+            set_hexpand: true,
+            connect_clicked: move |_| println!("Source language selection clicked")
+        },
+
+        target_lang_button = gtk4::Button {
+            set_label: TARGET_LANG.lock().unwrap().as_ref().map_or("Target...", |l| &l.name),
+            set_css_classes: &["google-translate-language-select-button", "target-lang"],
+            set_hexpand: true,
+            connect_clicked: move |_| println!("Target language selection clicked")
+        },
+
+        language_select_buttons = gtk4::Box {
+            set_hexpand: true,
+            
+            append: &source_lang_button,
+            gtk4::Label {
+                set_css_classes: &["google-translate-arrow"],
+                set_text: "→",
+                set_hexpand: true
+            },
+            append: &target_lang_button
+        },
+
         input_text_view = gtk4::TextView {
             set_wrap_mode: gtk4::WrapMode::WordChar,
             set_vexpand: true,
@@ -79,6 +136,8 @@ pub fn new() -> gtk4::Box {
             set_spacing: 8,
             set_hexpand: true,
             set_vexpand: true,
+
+            append: &language_select_buttons,
 
             gtk4::ScrolledWindow {
                 set_child: Some(&input_text_view)
@@ -117,14 +176,98 @@ pub fn new() -> gtk4::Box {
                             set_text: "Clear"
                         }
                     }
+                },
+
+                gtk4::Button {
+                    set_css_classes: &["google-translate-button"],
+                    connect_clicked: {
+                        let input_buffer = input_buffer.clone();
+                        let output_buffer = output_buffer.clone();
+                        let source_lang_button = source_lang_button.clone();
+                        let target_lang_button = target_lang_button.clone();
+                        let language_select_buttons = language_select_buttons.clone();
+                        let from_to_button_transition_provider = from_to_button_transition_provider.clone();
+                        let tx = tx.clone();
+
+                        move |_| if !is_working() {
+                            let input_text = input_buffer.text(&input_buffer.start_iter(), &input_buffer.end_iter(), false).to_string();
+                            let output_text = output_buffer.text(&output_buffer.start_iter(), &output_buffer.end_iter(), false).to_string();
+                            let source_lang_cloned = SOURCE_LANG.lock().unwrap().clone();
+                            let target_lang_cloned = TARGET_LANG.lock().unwrap().clone();
+
+                            change_source_language(target_lang_cloned, &tx);
+                            change_target_language(source_lang_cloned, &tx);
+
+                            input_buffer.set_text(&output_text);
+                            output_buffer.set_text(&input_text);
+
+                            // Button swap animation
+                            let source_lang_allocation = source_lang_button.allocation();
+                            let target_lang_allocation = target_lang_button.allocation();
+                            let buttons_box_allocation = language_select_buttons.allocation();
+                            
+                            from_to_button_transition_provider.load_from_data(&format!("
+                                .google-translate-language-select-button.source-lang {{
+                                    transition: none;
+                                    margin-left: {}px;
+                                    margin-right: -{}px;
+                                }}
+
+                                .google-translate-language-select-button.target-lang {{
+                                    transition: none;
+                                    margin-left: -{}px;
+                                    margin-right: {}px;
+                                }}",
+                                buttons_box_allocation.width() - source_lang_allocation.width() + 1,
+                                buttons_box_allocation.width() - source_lang_allocation.width() + 1,
+                                buttons_box_allocation.width() - target_lang_allocation.width() + 1,
+                                buttons_box_allocation.width() - target_lang_allocation.width() + 1
+                            ));
+
+                            gtk4::glib::timeout_add_local_once(Duration::from_millis(10), {
+                                let from_to_button_transition_provider = from_to_button_transition_provider.clone();
+                                move || from_to_button_transition_provider.load_from_data("
+                                    .google-translate-language-select-button.source-lang,
+                                    .google-translate-language-select-button.target-lang {
+                                        transition: margin-left 0.33s cubic-bezier(0.5, 0.8, 0, 1),
+                                                    margin-right 0.33s cubic-bezier(0.5, 0.8, 0, 1);
+                                        margin-left: 0px;
+                                        margin-right: 0px;
+                                    }
+                                ")
+                            });
+                        }
+                    },
+                            
+                    gtk4::Box {
+                        set_spacing: 4,
+                        set_halign: gtk4::Align::Center,
+
+                        gtk4::Label {
+                            set_css_classes: &["material-icons"],
+                            set_text: "swap_horiz"
+                        },
+
+                        gtk4::Label {
+                            set_text: "Swap"
+                        }
+                    }
                 }
             }
         }
     };
 
+    source_lang_button.style_context().add_provider(
+        &from_to_button_transition_provider,
+        gtk4::STYLE_PROVIDER_PRIORITY_APPLICATION
+    );
+
+    target_lang_button.style_context().add_provider(
+        &from_to_button_transition_provider,
+        gtk4::STYLE_PROVIDER_PRIORITY_APPLICATION
+    );
+
     input_buffer.connect_changed({
-        let source_lang = source_lang.clone();
-        let target_lang = target_lang.clone();
         let tx = tx.clone();
 
         move |buffer| {
@@ -138,16 +281,14 @@ pub fn new() -> gtk4::Box {
 
             let text = buffer.text(&buffer.start_iter(), &buffer.end_iter(), false).to_string();
             if !text.is_empty() {
-                let source_lang = source_lang.clone();
-                let target_lang = target_lang.clone();
                 let tx = tx.clone();
 
                 let timeout = gtk4::glib::timeout_add_local_once(Duration::from_millis(500), move || {
                     WORKER_TIMEOUT.lock().unwrap().take();
 
                     std::thread::spawn({
-                        let source_lang = source_lang.clone();
-                        let target_lang = target_lang.clone();
+                        let source_lang = SOURCE_LANG.lock().unwrap().clone();
+                        let target_lang = TARGET_LANG.lock().unwrap().clone();
                         let tx = tx.clone();
 
                         move || tokio::runtime::Runtime::new().unwrap().block_on(translate_future(
@@ -169,6 +310,8 @@ pub fn new() -> gtk4::Box {
     gtk4::glib::spawn_future_local({
         let output_buffer = output_buffer.clone();
         let input_text_view = input_text_view.clone();
+        let source_lang_button = source_lang_button.clone();
+        let target_lang_button = target_lang_button.clone();
 
         async move {
             while let Ok(event) = rx.recv().await {
@@ -181,11 +324,28 @@ pub fn new() -> gtk4::Box {
                     TranslationEvent::TranslationFinished(result) => {
                         if let Ok(res) = result {
                             output_buffer.set_text(&res.to.text);
+
+                            // Set the auto-detected language if applicable
+                            if SOURCE_LANG.lock().unwrap().as_ref().unwrap().code == "auto" {
+                                let mut auto_detected_lang = AUTO_DETECTED_LANG.lock().unwrap();
+
+                                *auto_detected_lang = Some(res.from.language.clone());
+
+                                source_lang_button.set_label(&format!("Auto ({})", res.from.language.name));
+                            }
                         } else {
                             output_buffer.set_text(&format!("Translation failed:\n{}", result.unwrap_err()));
                         }
 
                         input_text_view.set_editable(true);
+                    },
+
+                    TranslationEvent::SourceLanguageChanged(lang) => {
+                        source_lang_button.set_label(lang.as_ref().map_or("Source...", |l| &l.name));
+                    },
+
+                    TranslationEvent::TargetLanguageChanged(lang) => {
+                        target_lang_button.set_label(lang.as_ref().map_or("Target...", |l| &l.name));
                     }
                 }
             }
