637c07095df27a8ff2042a2262492eb5650d2d19
Inparsian <im@inpr.sn>
2025-12-14 23:29:42 -0500

    feat(sysstats/extended): add tabs

---
+++ src/widgets/bar/modules/sysstats/extended/mod.rs
@@ -0,0 +1,76 @@
+mod views {
+    pub mod overview;
+}
+
+use gtk4::prelude::*;
+
+use crate::widgets::common::tabs::{Tabs, TabsStack, TabSize};
+use self::views::overview::overview;
+
+#[derive(Clone)]
+pub struct CompactStatRow {
+    pub container: gtk4::Box,
+    pub value: gtk4::Label,
+    pub secondary_value: gtk4::Label, // e.g. cpu temp next to cpu usage
+}
+
+impl CompactStatRow {
+    pub fn new(label_text: &str, with_secondary: bool) -> Self {
+        let container = gtk4::Box::new(gtk4::Orientation::Horizontal, 4);
+        container.set_css_classes(&["bar-sysstats-compact-stat-row"]);
+        let label = gtk4::Label::new(None);
+        label.set_text(label_text);
+        label.set_hexpand(true);
+        label.set_halign(gtk4::Align::Start);
+        label.set_css_classes(&["bar-sysstats-compact-stat-row-label"]);
+        let value = gtk4::Label::new(None);
+        value.set_css_classes(&["bar-sysstats-compact-stat-row-value"]);
+        value.set_xalign(1.0);
+        let secondary_value = gtk4::Label::new(None);
+        secondary_value.set_css_classes(&["bar-sysstats-compact-stat-row-secondary-value"]);
+        secondary_value.set_xalign(1.0);
+
+        container.append(&label);
+        container.append(&value);
+
+        if with_secondary {
+            container.append(&secondary_value);
+        }
+
+        Self {
+            container,
+            value,
+            secondary_value,
+        }
+    }
+
+    pub fn set_value(&self, text: &str) {
+        self.value.set_text(text);
+    }
+
+    pub fn set_secondary_value(&self, text: &str) {
+        self.secondary_value.set_text(format!("({})", text).as_str());
+    }
+}
+
+pub fn extended() -> gtk4::Box {
+    let tabs = Tabs::new(TabSize::Normal, true);
+    tabs.current_tab.set(Some("overview".to_owned()));
+    tabs.add_tab("overview", "overview".to_owned(), Some("overview"));
+
+    let tabs_stack = TabsStack::new(&tabs, Some("bar-sysstats-extended-tabs-stack"));
+    tabs_stack.add_tab(Some("overview"), &overview());
+
+    view! {
+        widget = gtk4::Box {
+            set_css_classes: &["bar-sysstats-extended"],
+            set_orientation: gtk4::Orientation::Vertical,
+            set_spacing: 8,
+
+            append: &tabs.widget,
+            append: &tabs_stack.widget
+        },
+    }
+
+    widget
+}
--- src/widgets/bar/modules/sysstats/extended/views/overview.rs
+++ src/widgets/bar/modules/sysstats/extended/views/overview.rs
@@ -4,55 +4,10 @@ use futures_signals::signal::SignalExt as _;
 use crate::singletons::sysstats::SYS_STATS;
 use crate::singletons::sysstats::sensors::SENSORS;
 use crate::unit::bytes_to_gib;
-use super::SWAP_SHOW_THRESHOLD;
+use super::super::CompactStatRow;
+use super::super::super::SWAP_SHOW_THRESHOLD;
 
-#[derive(Clone)]
-pub struct CompactStatRow {
-    pub container: gtk4::Box,
-    pub value: gtk4::Label,
-    pub secondary_value: gtk4::Label, // e.g. cpu temp next to cpu usage
-}
-
-impl CompactStatRow {
-    pub fn new(label_text: &str, with_secondary: bool) -> Self {
-        let container = gtk4::Box::new(gtk4::Orientation::Horizontal, 4);
-        container.set_css_classes(&["bar-sysstats-compact-stat-row"]);
-        let label = gtk4::Label::new(None);
-        label.set_text(label_text);
-        label.set_hexpand(true);
-        label.set_halign(gtk4::Align::Start);
-        label.set_css_classes(&["bar-sysstats-compact-stat-row-label"]);
-        let value = gtk4::Label::new(None);
-        value.set_css_classes(&["bar-sysstats-compact-stat-row-value"]);
-        value.set_xalign(1.0);
-        let secondary_value = gtk4::Label::new(None);
-        secondary_value.set_css_classes(&["bar-sysstats-compact-stat-row-secondary-value"]);
-        secondary_value.set_xalign(1.0);
-
-        container.append(&label);
-        container.append(&value);
-
-        if with_secondary {
-            container.append(&secondary_value);
-        }
-
-        Self {
-            container,
-            value,
-            secondary_value,
-        }
-    }
-
-    pub fn set_value(&self, text: &str) {
-        self.value.set_text(text);
-    }
-
-    pub fn set_secondary_value(&self, text: &str) {
-        self.secondary_value.set_text(format!("({})", text).as_str());
-    }
-}
-
-pub fn extended() -> gtk4::Box {
+pub fn overview() -> gtk4::Box {
     let cpu_stat_row = CompactStatRow::new("CPU", true);
     let mem_stat_row = CompactStatRow::new("RAM", true);
     let swap_stat_row = CompactStatRow::new("SWAP", true);
@@ -61,7 +16,6 @@ pub fn extended() -> gtk4::Box {
 
     view! {
         widget = gtk4::Box {
-            set_css_classes: &["bar-sysstats-extended"],
             set_orientation: gtk4::Orientation::Vertical,
             set_spacing: 8,
 
