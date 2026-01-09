075d689fb239d8458c1979a976b533f19a535f62
Inparsian <im@inpr.sn>
2026-01-08 19:41:49 -0500

    feat(left_sidebar): add expansion feature

--- src/widgets/windows/sidebar_left/mod.rs
+++ src/widgets/windows/sidebar_left/mod.rs
@@ -19,6 +19,18 @@ pub fn new(application: &libadwaita::Application) -> PopupWindow {
     tabs_stack.add_tab(Some("ai"), &modules::ai::new());
 
     view! {
+        left_sidebar_expand_button_label = gtk4::Label {
+            set_css_classes: &["left-sidebar-expand-button-icon"],
+            set_label: "expand_content",
+        },
+
+        left_sidebar_expand_button = gtk4::Button {
+            set_css_classes: &["left-sidebar-expand-button"],
+            set_halign: gtk4::Align::End,
+            set_hexpand: true,
+            set_child: Some(&left_sidebar_expand_button_label),
+        },
+
         left_sidebar_box = gtk4::Box {
             set_css_classes: &["left-sidebar-box"],
             set_orientation: gtk4::Orientation::Vertical,
@@ -26,19 +38,46 @@ pub fn new(application: &libadwaita::Application) -> PopupWindow {
             set_hexpand: true,
             set_vexpand: true,
 
-            append: &tabs.widget,
-            append: &tabs_stack.widget
+            gtk4::Box {
+                set_orientation: gtk4::Orientation::Horizontal,
+                set_spacing: 0,
+                append: &tabs.widget,
+                append: &left_sidebar_expand_button,
+            },
+            append: &tabs_stack.widget,
         },
     };
 
+    let toggle_expand = {
+        let left_sidebar_box = left_sidebar_box.clone();
+        move || if left_sidebar_box.has_css_class("expanded") {
+            left_sidebar_box.remove_css_class("expanded");
+            left_sidebar_expand_button_label.set_label("expand_content");
+        } else {
+            left_sidebar_box.add_css_class("expanded");
+            left_sidebar_expand_button_label.set_label("collapse_content");
+        }
+    };
+
+    left_sidebar_expand_button.connect_clicked({
+        let toggle_expand = toggle_expand.clone();
+        move |_| toggle_expand()
+    });
+
     ipc::listen_for_messages_local(move |message| {
         let mut split_whitespace_iterator = message.split_whitespace();
-        if let Some(message) = split_whitespace_iterator.next()
-            && message == "change_left_sidebar_tab"
-            && let Some(tab) = split_whitespace_iterator.next()
-            && tabs.items.try_borrow().is_ok_and(|vec| vec.iter().any(|t| t.name == tab))
-        {
-            tabs.current_tab.set(Some(tab.to_owned()));
+        if let Some(message) = split_whitespace_iterator.next() {
+            match message {
+                "change_left_sidebar_tab" => if let Some(tab) = split_whitespace_iterator.next()
+                    && tabs.items.try_borrow().is_ok_and(|vec| vec.iter().any(|t| t.name == tab))
+                {
+                    tabs.current_tab.set(Some(tab.to_owned()));
+                },
+
+                "toggle_left_sidebar_expanded" => toggle_expand(),
+                
+                _ => {},
+            }
         }
     });
 
